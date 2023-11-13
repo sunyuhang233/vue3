01aab82385f9f161a38cfa548f0d11e90a706ae5
hang <2283582431@qq.com>
2023-11-13 20:14:10 +0800

    element-plus 新主题的立即生效

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import useAppStore from "@/stores/app";
+import { generateNewStyle, writeNewStyle } from "@/utils/theme";
 
+const appStore = useAppStore();
+generateNewStyle(appStore.mainColor).then(newStyleText => {
+  writeNewStyle(newStyleText);
+});
 // 获取.env.development 的 VUE_APP_BASE_API
 console.log(import.meta.env.VITE_API_URL);
 </script>
