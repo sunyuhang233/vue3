2f4b9b8e2097ff3f447aa501815d3494f9914755
hang <2283582431@qq.com>
2023-11-13 19:06:45 +0800

    根据动态数据，渲染面包屑

--- src/components/main-header/c-cpns/header-breadcrumb.vue
+++ src/components/main-header/c-cpns/header-breadcrumb.vue
@@ -1,18 +1,17 @@
 <template>
   <el-breadcrumb class="breadcrumb" separator="/">
-    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-    <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
-    <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-    <!-- 面包屑的最后一项 -->
-    <el-breadcrumb-item>
-      <span class="no-redirect">活动详情</span>
+    <el-breadcrumb-item v-for="(item, index) in breadcrumbData" :key="item.path">
+      <!-- 不可点击项 -->
+      <span v-if="index === breadcrumbData.length - 1" class="no-redirect">{{ item.meta.title }}</span>
+      <!-- 可点击项 -->
+      <a v-else class="redirect" @click.prevent="onLinkClick(item)">{{ item.meta.title }}</a>
     </el-breadcrumb-item>
   </el-breadcrumb>
 </template>
 
 <script setup lang="ts">
 import { ref, watch } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 const route = useRoute();
 // 生成数组数据
 const breadcrumbData = ref<any[]>([]);
@@ -30,6 +29,12 @@ watch(
     immediate: true
   }
 );
+// 处理点击事件
+const router = useRouter();
+const onLinkClick = (item: any) => {
+  console.log(item);
+  router.push(item.path);
+};
 </script>
 
 <style lang="scss" scoped>
