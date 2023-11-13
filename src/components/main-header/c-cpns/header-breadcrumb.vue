33134db8b44b932c702f62d18f35b78c760fa3ae
hang <2283582431@qq.com>
2023-11-13 19:08:50 +0800

    Vue3 动画处理

--- src/components/main-header/c-cpns/header-breadcrumb.vue
+++ src/components/main-header/c-cpns/header-breadcrumb.vue
@@ -1,11 +1,13 @@
 <template>
   <el-breadcrumb class="breadcrumb" separator="/">
-    <el-breadcrumb-item v-for="(item, index) in breadcrumbData" :key="item.path">
-      <!-- 不可点击项 -->
-      <span v-if="index === breadcrumbData.length - 1" class="no-redirect">{{ item.meta.title }}</span>
-      <!-- 可点击项 -->
-      <a v-else class="redirect" @click.prevent="onLinkClick(item)">{{ item.meta.title }}</a>
-    </el-breadcrumb-item>
+    <transition-group name="breadcrumb">
+      <el-breadcrumb-item v-for="(item, index) in breadcrumbData" :key="item.path">
+        <!-- 不可点击项 -->
+        <span v-if="index === breadcrumbData.length - 1" class="no-redirect">{{ item.meta.title }}</span>
+        <!-- 可点击项 -->
+        <a v-else class="redirect" @click.prevent="onLinkClick(item)">{{ item.meta.title }}</a>
+      </el-breadcrumb-item>
+    </transition-group>
   </el-breadcrumb>
 </template>
 
@@ -48,5 +50,20 @@ const onLinkClick = (item: any) => {
     color: #97a8be;
     cursor: text;
   }
+}
+
+.breadcrumb-enter-active,
+.breadcrumb-leave-active {
+  transition: all 0.5s;
+}
+
+.breadcrumb-enter-from,
+.breadcrumb-leave-active {
+  opacity: 0;
+  transform: translateX(20px);
+}
+
+.breadcrumb-leave-active {
+  position: absolute;
 }
 </style>
